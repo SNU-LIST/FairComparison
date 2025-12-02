dd4acfb2ac451c5446ad9194f92a6d896aeaa2c3
Chungseok Oh <csoh353@gmail.com>
2025-12-02 13:55:17 +0900

    Delete code_for_dataset_generation directory

--- code_for_dataset_generation/util/Grappa4JaeYeon/fft2c.m
+++
@@ -1,5 +0,0 @@
-function res = ifft2c(x)
-fctr = size(x,1)*size(x,2);
-for n=1:size(x,3)
-res(:,:,n) = ifftshift(fft2(fftshift(x(:,:,n))))/sqrt(fctr);
-end
--- code_for_dataset_generation/util/chungseok/wsl.m
+++
@@ -1,8 +0,0 @@
-function [] = wsl(cmd)
-    fprintf(['...' cmd '\n'])
-    if ispc
-        system(['bash -c "source ~/.profile &&' cmd '"']);
-    elseif isunix
-        system(cmd)
-    end    
-end
